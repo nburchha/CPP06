9e097b6b0e68c357dfebb14db9985fa14f248068
nburchha <niklas@burchhardt.de>
2024-07-28 14:22:54 +0200

    ex02 done, not tested on linux, not checked for leaks

---
+++ ex02/main.cpp
@@ -0,0 +1,61 @@
+#include "Base.hpp"
+
+Base* generate(void)
+{
+	static bool seeded = false;
+	if (!seeded)
+	{
+		seeded = true;
+		srand(static_cast<unsigned int>(time(NULL)));
+	}
+	int random = rand() % 3;
+	switch (random)
+	{
+		case 0:
+			return new A();
+		case 1:
+			return new B();
+		case 2:
+			return new C();
+	}
+	return nullptr;
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::bad_cast&) {}
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	} catch (std::bad_cast&) {}
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	} catch (std::bad_cast&) {}
+}
+
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Base* base = generate();
+		identify(base);
+		identify(*base);
+		delete base;
+	}
+	return 0;
+}
